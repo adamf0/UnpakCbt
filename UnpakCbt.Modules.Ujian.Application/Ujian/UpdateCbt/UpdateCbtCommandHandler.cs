9b490d532eb9b1e526c710fb135262105d0af256
!Github@251423! <studiocode52@gmail.com>
2025-03-17 17:10:26 +0700

    panambahan logging di update cbt

--- UnpakCbt.Modules.Ujian.Application/Ujian/UpdateCbt/UpdateCbtCommandHandler.cs
+++ UnpakCbt.Modules.Ujian.Application/Ujian/UpdateCbt/UpdateCbtCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using Microsoft.Extensions.Logging;
+using System.Globalization;
 using UnpakCbt.Common.Application.Messaging;
 using UnpakCbt.Common.Domain;
 using UnpakCbt.Modules.JadwalUjian.Domain.JadwalUjian;
@@ -15,14 +16,19 @@ namespace UnpakCbt.Modules.Ujian.Application.Ujian.UpdateCbt
     ICbtRepository cbtRepository,
     IUnitOfWork unitOfWork,
     IJadwalUjianApi jadwalUjianApi,
-    ITemplateJawabanApi templateJawabanApi)
+    ITemplateJawabanApi templateJawabanApi,
+    ILogger<UpdateCbtCommandHandler> logger)
     : ICommandHandler<UpdateCbtCommand>
     {
         public async Task<Result> Handle(UpdateCbtCommand request, CancellationToken cancellationToken)
         {
+            logger.LogInformation("Received command with parameters: {@Request}", request);
+
             Domain.Ujian.Ujian? existingUjian = await ujianRepository.GetAsync(request.Uuid, cancellationToken);
+            logger.LogInformation("existingUjian: {@existingUjian}", existingUjian);
 
             JadwalUjianResponse? jadwalUjian = await jadwalUjianApi.GetAsync(request.IdJadwalUjian, cancellationToken);
+            logger.LogInformation("jadwalUjian: {@jadwalUjian}", jadwalUjian);
             if (jadwalUjian is null)
             {
                 return Result.Failure<Guid>(JadwalUjianErrors.NotFound(request.IdJadwalUjian));
@@ -79,6 +85,7 @@ namespace UnpakCbt.Modules.Ujian.Application.Ujian.UpdateCbt
             if (request?.IdJawabanBenar != null)
             {
                 TemplateJawabanResponse? templateJawaban = await templateJawabanApi.GetAsync(request.IdJawabanBenar, cancellationToken);
+                logger.LogInformation("templateJawaban: {@templateJawaban}", templateJawaban);
 
                 if (templateJawaban is null)
                 {
@@ -90,8 +97,11 @@ namespace UnpakCbt.Modules.Ujian.Application.Ujian.UpdateCbt
             if (JawabanBenar==null || JawabanBenar <=0) {
                 return Result.Failure<Guid>(UjianErrors.IdJawabanBenarNotFound(request?.IdJawabanBenar??Guid.Empty));
             }
+            logger.LogInformation("JawabanBenar: {@JawabanBenar}", JawabanBenar);
 
             Cbt existingCbt = await cbtRepository.GetAsync(request.Uuid);
+            logger.LogInformation("existingCbt: {@existingCbt}", existingCbt);
+
             Result<Cbt> currentCbt = Cbt.Update(existingCbt)
                          .ChangeJawabanBenar(JawabanBenar)
                          .Build();
